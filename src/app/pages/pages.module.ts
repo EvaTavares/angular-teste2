6a85634427d1f05286902c0d77bcdc2065e75b07
Eva Tavares <eva.programadora@gmail.com>
2024-02-07 14:29:01 -0300

    pages module

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListRenderComponent } from './list-render/list-render.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListRenderComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+})
+export class PagesModule { }
