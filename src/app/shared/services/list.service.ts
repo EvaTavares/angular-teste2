a81e47347c9c60504efec8845afa334ba4273ae6
Eva Tavares <eva.programadora@gmail.com>
2024-02-07 17:52:21 -0300

    add: apiURL

--- src/app/shared/services/list.service.ts
+++ src/app/shared/services/list.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Animal } from '../models/Animal';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListService {
+  private apiURL: string = 'http://localhost:3000'
 
-  constructor() { }
+  constructor(private http: HttpClient ) { }
 
   remove(animals: Animal[], animal: Animal){
     return animals.filter((a)=> animal.name !== a.name)
+  }
+
+  getAll(): Observable<Animal[]>{
+    const url: string = `${this.apiURL}/animals`;
+    return this.http.get<Animal[]>(url)
   }
 }
