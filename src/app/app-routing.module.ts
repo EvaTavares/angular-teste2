13b6e4fc7fe3e2203116ca58efc9dcee393b9a48
Eva Tavares <eva.programadora@gmail.com>
2024-02-08 11:59:46 -0300

    add: rotas homeComponent e ItemDetailComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListRenderComponent } from './pages/list-render/list-render.component';
+import { ItemDetailComponent } from './pages/item-detail/item-detail.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: ListRenderComponent
-  }
+  { path: '',component: HomeComponent},
+  { path: 'list',component: ListRenderComponent},
+  { path: 'list/:id', component: ItemDetailComponent},
 ];
 
 @NgModule({
