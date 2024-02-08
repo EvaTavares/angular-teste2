e988e29742a5d948a4ed78c60c3a1a48b7e9a82c
Eva Tavares <eva.programadora@gmail.com>
2024-02-08 11:57:33 -0300

    add: itemDetail e home componentes

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListRenderComponent } from './list-render/list-render.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    ListRenderComponent
+    ListRenderComponent,
+    ItemDetailComponent,
+    HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
 })
 export class PagesModule { }
