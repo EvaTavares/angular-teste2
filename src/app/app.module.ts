a990ba084e4145620230f96488a6cbece21a51de
Eva Tavares <eva.programadora@gmail.com>
2024-02-08 11:57:05 -0300

    add: RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './pages/pages.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
